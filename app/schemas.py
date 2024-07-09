68c7d71328894fc37e51d2ee72932b43a6950d03
yusufcanekin <117827353+yusufcanekin@users.noreply.github.com>
2024-07-09 16:28:54 +0300

    Delete app directory

--- app/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-# This class creates a schema. When you pass an object of this class to a get method, 
-# data fields of that object must be provided in the specified type.
-class Post(BaseModel):
-    title: str
-    content: Optional[str] = None # Makes the content optional. If it is not provided, it is null by default.
-    id: int
